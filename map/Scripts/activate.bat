289a5e95f7530949de24324dff5b160da70d8cf6
felician schwarz <53663553+MartinRouterKing64@users.noreply.github.com>
2022-10-09 19:03:16 +0200

    added virtual environment

--- map/Scripts/activate.bat
+++ map/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nutzer\Desktop\John Cowley\map
+set VIRTUAL_ENV=C:\Users\Nutzer\Desktop\John Cowley\memorise-anything-project\map
 
 if not defined PROMPT set PROMPT=$P$G
 
