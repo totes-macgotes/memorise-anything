c37b049e3aabeed2c4769d5647a31cfdb264cd2b
felician schwarz <53663553+MartinRouterKing64@users.noreply.github.com>
2022-10-09 22:31:46 +0200

    added login and registerform

--- forms.py
+++ forms.py
@@ -1,19 +1,26 @@
-import os
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, PasswordField, BooleanField, SubmitField, SelectField, DateField
 from wtforms.validators import DataRequired, Length, Email, NumberRange, EqualTo
 
+import os
+import pycountry
+
+class CountrySelectField(SelectField):
+    def __init__(self, *args, **kwargs):
+        super(CountrySelectField, self).__init__(*args, **kwargs)
+        self.choices = [(country.alpha2, country.name) for country in pycountry.countries]
 
 class LoginForm(FlaskForm):
-    username = StringField("Benutzername", validators=[DataRequired()])
-    password = PasswordField("Passwort", validators=[DataRequired()])
-    submit = SubmitField("Login")
-
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    submit = SubmitField("login")
 
 class RegisterForm(FlaskForm):
-    username = StringField("Benutzername", validators=[DataRequired(message="*erforderlich")])
-    user_email = StringField("E-Mail-Adresse", validators=[DataRequired(message="*erforderlich")])
-    password = PasswordField("Passwort", validators=[DataRequired(message="*erforderlich")])
-    confirm_password = PasswordField("Passwort bestätigen", validators=[DataRequired(message="*erforderlich"), EqualTo("password", message="Das Passwort stimmt nicht überein.")])
-    submit = SubmitField("Registrieren")
+    first_name = StringField("first name", validators=[DataRequired(message="*erforderlich")])
+    last_name = StringField("last name", validators=[DataRequired(message="*erforderlich")])
+    user_email = StringField("e-mail-address", validators=[DataRequired(message="*erforderlich")])
+    username = StringField("username", validators=[DataRequired(message="*erforderlich")])
+    country = CountrySelectField("username", validators=[DataRequired(message="*erforderlich")])
+    password = PasswordField("password", validators=[DataRequired(message="*erforderlich")])
+    confirm_password = PasswordField("confirm password", validators=[DataRequired(message="*erforderlich"), EqualTo("password", message="Das Passwort stimmt nicht überein.")])
+    submit = SubmitField("register")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from forms import LoginForm, RegisterForm
+
+import string
+import random
 
 from app_secrets import FLASK_APP_SECRET_KEY
 
@@ -13,15 +16,34 @@ def menu():
     return "<p>Menu Page</p>"
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
 	form = LoginForm()
+
+	if form.validate_on_submit():
+		return redirect(url_for("menu"))
+
 	return render_template("login.html", form=form)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-	return "login page"
+	form = RegisterForm()
+
+	if form.validate_on_submit():
+		return redirect(url_for("menu"))
+
+	lower = string.ascii_lowercase
+	upper = string.ascii_uppercase
+	num = string.digits
+	symbols = string.punctuation
+
+	all = lower + upper + num + symbols
+	temp = random.sample(all, 12)
+	recommended_pw = "".join(temp)
+
+
+	return render_template("register.html", form=form, recommended_pw=recommended_pw)
 
 
 if __name__ == "__main__":
